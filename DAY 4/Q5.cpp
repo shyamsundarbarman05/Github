046236817c4c7c35fa1d6579cf0b88a98735f56d
Shyamsundar Barman <shyamsundarbarman373@gmail.com>
2024-11-07 21:59:37 +0530

    modified

--- DAY 4/Q5.cpp
+++ DAY 4/Q5.cpp
@@ -1,12 +1,24 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main()
-{
-    int arr[5] = {1, 2, 3, 4, 5};
-    int *ptr = arr;
-    for (int i = 0; i < 5; i++)
-    {
-        cout << *(ptr + i) << " ";
+float si(int p,int t,float r=15){
+    return (p*t*r)/100;
+}
+int main(){
+    int a,b;
+    cout<<"Enter the principle amount: "<<endl;
+    cout<<"Enter the time in years: "<<endl;
+    cin>>a>>b;
+    cout<<"Do you want to change the interest rate(y/n): "<<endl;
+    char y,key;
+    cin>>key;
+    float c;
+    if(key=='y'){
+        cout<<"Enter the interest rate: "<<endl;
+        cin>>c;
+        cout<<"The total interest is: "<<si(a,b,c);
+    }
+    else{
+        cout<<"The total interest is: "<<si(a,b);
     }
     return 0;
 }
