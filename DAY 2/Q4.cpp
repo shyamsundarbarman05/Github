43241a6267bea99972925a5b9f50cba346a38062
Shyamsundar Barman <shyamsundarbarman373@gmail.com>
2024-11-07 21:41:37 +0530

    modified

--- DAY 2/Q4.cpp
+++ DAY 2/Q4.cpp
@@ -1,55 +1,40 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-class Employee
-{
-    string name;
-    int id;
-public:
-    void inputDetails()
-    {
-        cout << "Enter employee name: ";
-        cin.ignore(); // To ignore any leftover newline character in the input buffer
-        getline(cin, name);
-        cout << "Enter employee ID: ";
-        cin >> id;
+
+float gradeToPoint(char grade) {
+    switch (grade) {
+        case 'A': return 10.0;
+        case 'B': return 8.0;
+        case 'C': return 6.0;
+        case 'D': return 5.0;
+        case 'E': return 4.0;
+        case 'F': return 0.0;
+        default: return 0.0;
     }
-    void displayDetails() const
-    {
-        cout << "Name: " << name << ", ID: " << id << endl;
+}
+
+int main() {
+    int numSubjects;
+    cout << "Enter the number of subjects: ";
+    cin >> numSubjects;
+
+    int credits[numSubjects];
+    char grades[numSubjects];
+    float totalPoints = 0.0;
+    int totalCredits = 0;
+
+    for (int i = 0; i < numSubjects; i++) {
+        cout << "Enter the credits for subject " << i + 1 << ": ";
+        cin >> credits[i];
+        cout << "Enter the grade (A/B/C/D/E/F) for subject " << i + 1 << ": ";
+        cin >> grades[i];
+
+        totalPoints += credits[i] * gradeToPoint(grades[i]);
+        totalCredits += credits[i];
     }
-};
-class Department
-{
-    vector<Employee> employees;
-public:
-    void addEmployee()
-    {
-        Employee emp;
-        emp.inputDetails();
-        employees.push_back(emp);
-    }
-    void displayAllEmployees() const
-    {
-        cout << "Employee details:\n";
-        for (const auto &emp : employees)
-        {
-            emp.displayDetails();
-        }
-        cout << "Total number of employees: " << employees.size() << endl;
-    }
-};
-int main()
-{
-    Department dept;
-    int num;
-    cout << "Enter the number of employees: ";
-    cin >> num;
-    for (int i = 0; i < num; ++i)
-    {
-        cout << "Entering details for employee " << i + 1 << ":\n";
-        dept.addEmployee();
-    }
-    dept.displayAllEmployees();
+
+    float sgpa = totalPoints / totalCredits;
+    cout << "SGPA: " << sgpa << endl;
+
     return 0;
 }
