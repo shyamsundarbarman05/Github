caeab3c61c01b6fa6f8c7b0bd76da050d668fba6
Shyamsundar Barman <shyamsundarbarman373@gmail.com>
2024-11-07 21:54:09 +0530

    modified

--- DAY 4/Q2.cpp
+++ DAY 4/Q2.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 using namespace std;
-int fibonacci(int n)
-{
-    if (n <= 1)
-        return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
-}
-int main()
-{
-    int n;
-    cout << "Enter the number of terms: ";
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        cout << fibonacci(i) << " ";
+
+void towerOfHanoi(int n, char source, char auxiliary, char destination) {
+    if (n == 1) {
+        cout << "Move disk 1 from " << source << " to " << destination << endl;
+        return;
     }
+    towerOfHanoi(n - 1, source, destination, auxiliary);
+    cout << "Move disk " << n << " from " << source << " to " << destination << endl;
+    towerOfHanoi(n - 1, auxiliary, source, destination);
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of disks: ";
+    cin >> n;
+
+    towerOfHanoi(n, 'A', 'B', 'C');
+
     return 0;
 }
