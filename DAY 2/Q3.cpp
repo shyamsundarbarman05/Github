29373e44b4c0d93635ca9519a26507ddb1e3c8ad
Shyamsundar Barman <shyamsundarbarman373@gmail.com>
2024-11-07 21:38:38 +0530

    modified

--- DAY 2/Q3.cpp
+++ DAY 2/Q3.cpp
@@ -1,53 +1,27 @@
 #include <iostream>
 using namespace std;
-class SectionA; // Forward declaration
-class SectionB
-{
-    int marks[5];
-public:
-    void inputMarks()
-    {
-        cout << "Enter marks for 5 students in Section B:\n";
-        for (int i = 0; i < 5; ++i)
-        {
-            cout << "Student " << i + 1 << ": ";
-            cin >> marks[i];
-        }
-    }
-    friend float calculateAverage(SectionA, SectionB); // Friend function declaration
-};
-class SectionA
-{
-    int marks[5];
-public:
-    void inputMarks()
-    {
-        cout << "Enter marks for 5 students in Section A:\n";
-        for (int i = 0; i < 5; ++i)
-        {
-            cout << "Student " << i + 1 << ": ";
-            cin >> marks[i];
-        }
-    }
-    friend float calculateAverage(SectionA, SectionB); // Friend function declaration
-};
-float calculateAverage(SectionA a, SectionB b)
-{
-    int total = 0;
-    for (int i = 0; i < 5; ++i)
-    {
-        total += a.marks[i];
-        total += b.marks[i];
-    }
-    return total / 10.0; // Calculate average of all 10 students
-}
 int main()
 {
-    SectionA sectionA;
-    SectionB sectionB;
-    sectionA.inputMarks();
-    sectionB.inputMarks();
-    float avg = calculateAverage(sectionA, sectionB);
-    cout << "The average marks of 10 students (from both sections) is: " << avg << endl;
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+    int vowels = 0, consonants = 0;
+    for (char c : str)
+    {
+        switch (tolower(c))
+        {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            vowels++;
+            break;
+        default:
+            consonants++;
+        }
+    }
+    cout << "Number of vowels: " << vowels << endl;
+    cout << "Number of consonants: " << consonants << endl;
     return 0;
 }
