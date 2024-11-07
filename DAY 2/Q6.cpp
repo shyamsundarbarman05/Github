36823e4b0ea5df28e810ed1a34919a6933496b4e
Shyamsundar Barman <shyamsundarbarman373@gmail.com>
2024-11-07 21:46:57 +0530

    modified

--- DAY 2/Q6.cpp
+++ DAY 2/Q6.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
+
+void stringCopy(char dest[], const char src[]) {
+    int i = 0;
+    while (src[i] != '\0') {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+}
+
+void stringConcat(char dest[], const char src[]) {
+    int i = 0;
+    while (dest[i] != '\0') {
+        i++;
+    }
+    int j = 0;
+    while (src[j] != '\0') {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
+}
+
+int stringCompare(const char str1[], const char str2[]) {
+    int i = 0;
+    while (str1[i] != '\0' && str2[i] != '\0') {
+        if (str1[i] != str2[i]) {
+            return str1[i] - str2[i];
+        }
+        i++;
+    }
+    return str1[i] - str2[i];
+}
 
 int main() {
     char str1[100], str2[100], result[200];
@@ -12,15 +45,15 @@ int main() {
     cin.getline(str2, 100);
 
     // Copy str1 to result
-    strcpy(result, str1);
+    stringCopy(result, str1);
     cout << "Copy of first string: " << result << endl;
 
     // Concatenate str1 and str2
-    strcat(result, str2);
+    stringConcat(result, str2);
     cout << "Concatenated string: " << result << endl;
 
     // Compare str1 and str2
-    int comparison = strcmp(str1, str2);
+    int comparison = stringCompare(str1, str2);
     if (comparison == 0) {
         cout << "The strings are equal." << endl;
     } else if (comparison < 0) {
